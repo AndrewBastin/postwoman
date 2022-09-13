bda9b29c53f3ffd0299ee3ed93828b20d26239ed
Andrew Bastin <andrewbastin.k@gmail.com>
2022-09-13 21:04:08 +0530

    feat: introduce delay on progress bar animation on page transition

--- packages/hoppscotch-app/src/modules/loadingbar.ts
+++ packages/hoppscotch-app/src/modules/loadingbar.ts
@@ -1,15 +1,42 @@
 import { HoppModule } from "."
 import NProgress from "nprogress"
 
-export const startPageProgress = () => {
+let deferedProgressHandle: ReturnType<typeof setTimeout> | null = null
+
+/**
+ * Starts animating the global progress bar
+ * @param deferTime How much time to defer the global progress bar rendering to
+ */
+export const startPageProgress = (deferTime?: number) => {
+  if (deferedProgressHandle) clearTimeout(deferedProgressHandle)
+
+  // If deferTime is specified, queue it
+  if (deferTime !== undefined) {
+    deferedProgressHandle = setTimeout(() => {
+      NProgress.start()
+    }, deferTime)
+
+    return
+  }
+
   NProgress.start()
 }
 
 export const completePageProgress = () => {
+  if (deferedProgressHandle) {
+    clearTimeout(deferedProgressHandle)
+    deferedProgressHandle = null
+  }
+
   NProgress.done()
 }
 
 export const removePageProgress = () => {
+  if (deferedProgressHandle) {
+    clearTimeout(deferedProgressHandle)
+    deferedProgressHandle = null
+  }
+
   NProgress.remove()
 }
 
@@ -20,7 +47,7 @@ export default <HoppModule>{
   onBeforeRouteChange(to, from) {
     // Show progressbar on page change
     if (to.path !== from.path) {
-      startPageProgress()
+      startPageProgress(500)
     }
   },
   onAfterRouteChange() {
