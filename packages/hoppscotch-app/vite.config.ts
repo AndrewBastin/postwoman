458b1abe2e90244534920bb290bdfa27219ea647
Andrew Bastin <andrewbastin.k@gmail.com>
2022-09-17 14:37:28 +0530

    chore: fix issues with prod build of the app

--- packages/hoppscotch-app/vite.config.ts
+++ packages/hoppscotch-app/vite.config.ts
@@ -9,9 +9,6 @@ import Inspect from "vite-plugin-inspect"
 import WindiCSS from "vite-plugin-windicss"
 import Checker from "vite-plugin-checker"
 import { VitePWA } from "vite-plugin-pwa"
-import NodePolyfills from "rollup-plugin-polyfill-node"
-import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill"
-import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill"
 import Pages from "vite-plugin-pages"
 import Layouts from "vite-plugin-vue-layouts"
 import IconResolver from "unplugin-icons/resolver"
@@ -48,30 +45,9 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-  optimizeDeps: {
-    esbuildOptions: {
-      // Node.js global to browser globalThis
-      define: {
-        global: "globalThis",
-      },
-      // Enable esbuild polyfill plugins
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          process: true,
-          buffer: true,
-        }),
-        NodeModulesPolyfillPlugin(),
-      ],
-    },
-  },
   build: {
     sourcemap: true,
-    commonjsOptions: {
-      exclude: ["paho-mqtt"],
-    },
-    rollupOptions: {
-      plugins: [NodePolyfills()],
-    },
+    emptyOutDir: true,
   },
   resolve: {
     alias: {
@@ -83,40 +59,8 @@ export default defineConfig({
       "@functional": path.resolve(__dirname, "./src/helpers/functional"),
       "@workers": path.resolve(__dirname, "./src/workers"),
 
-      // Excerpt from: https://gist.github.com/sujit-baniya/759104dbbd9fa76d20a042108bad0f78
-      // This Rollup aliases are extracted from @esbuild-plugins/node-modules-polyfill,
-      // see https://github.com/remorses/esbuild-plugins/blob/master/node-modules-polyfill/src/polyfills.ts
-      // process and buffer are excluded because already managed
-      // by node-globals-polyfill
-      util: "rollup-plugin-node-polyfills/polyfills/util",
-      sys: "util",
-      events: "rollup-plugin-node-polyfills/polyfills/events",
       stream: "stream-browserify",
-      path: "rollup-plugin-node-polyfills/polyfills/path",
-      querystring: "rollup-plugin-node-polyfills/polyfills/qs",
-      punycode: "rollup-plugin-node-polyfills/polyfills/punycode",
-      url: "rollup-plugin-node-polyfills/polyfills/url",
-      http: "rollup-plugin-node-polyfills/polyfills/http",
-      https: "rollup-plugin-node-polyfills/polyfills/http",
-      os: "rollup-plugin-node-polyfills/polyfills/os",
-      assert: "rollup-plugin-node-polyfills/polyfills/assert",
-      constants: "rollup-plugin-node-polyfills/polyfills/constants",
-      _stream_duplex:
-        "rollup-plugin-node-polyfills/polyfills/readable-stream/duplex",
-      _stream_passthrough:
-        "rollup-plugin-node-polyfills/polyfills/readable-stream/passthrough",
-      _stream_readable:
-        "rollup-plugin-node-polyfills/polyfills/readable-stream/readable",
-      _stream_writable:
-        "rollup-plugin-node-polyfills/polyfills/readable-stream/writable",
-      _stream_transform:
-        "rollup-plugin-node-polyfills/polyfills/readable-stream/transform",
-      timers: "rollup-plugin-node-polyfills/polyfills/timers",
-      console: "rollup-plugin-node-polyfills/polyfills/console",
-      vm: "rollup-plugin-node-polyfills/polyfills/vm",
-      zlib: "rollup-plugin-node-polyfills/polyfills/zlib",
-      tty: "rollup-plugin-node-polyfills/polyfills/tty",
-      domain: "rollup-plugin-node-polyfills/polyfills/domain",
+      util: "util",
     },
   },
   plugins: [
